175fa10acf5dc2a84e0ca9f0985bd9dc246e0ded
Leilani Hagen <hagen.leilani@gmail.com>
2021-02-21 16:46:35 -0800

    Init commit, project template.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:minimalist_flutter_template/main.dart';
+import 'package:hospital_stay_helper/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
