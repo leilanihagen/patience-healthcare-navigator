075b3e0c6a324eb58209ca617374803acf388652
Leilani Hagen <hagen.leilani@gmail.com>
2021-02-27 18:57:28 -0800

    Bottom nav visuals

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,8 +14,27 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("Hospital Stay Helper - Home"),
       ),
+      bottomNavigationBar:
+          BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(IconData(62421, fontFamily: 'MaterialIcons')),
+          label: 'Guidelines',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(IconData(59162, fontFamily: 'MaterialIcons')),
+          label: 'Your Profile',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(IconData(59828, fontFamily: 'MaterialIcons')),
+          label: 'Search Health Services',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(IconData(0xe857, fontFamily: 'MaterialIcons')),
+          label: 'Find In-Network Hospitals',
+        ),
+      ]),
     );
   }
 }
