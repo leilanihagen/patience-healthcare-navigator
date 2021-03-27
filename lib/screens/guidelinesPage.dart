1449d0757b78d9fcd9bba6a1329a54cce27de83c
Leilani Hagen <hagen.leilani@gmail.com>
2021-03-27 11:35:44 -0700

    Updated Before and During pgs w/ Suzies suggestions +CollectionsPage

--- lib/screens/guidelinesPage.dart
+++ lib/screens/guidelinesPage.dart
@@ -225,6 +225,8 @@ class RootCategoriesPage extends StatelessWidget {
                   "I've received a surprise medical bill"),
               renderClickableSituationCard(context, TermsPage(),
                   "I want to learn healthcare terms and definitions"),
+              renderClickableSituationCard(context, CollectionsPage(),
+                  "My medical bill/debt has been sent to a collections agency"),
             ],
           ),
         ));
@@ -238,19 +240,24 @@ class BeforeStayPage extends StatelessWidget {
   final RootCategoriesPage rootCategoriesPage;
   final List<String> guidelinesTitles = [
     "Make a detailed visit-plan with your provider",
-    "Make a list of services/procedures you expect to receive",
-    "Make a list of all hospital staff you plan to receive treatment from",
-    "Call your insurance provider",
+    "Create a file or one place to store all information related to your hospital visit",
+    "Call your insurance provider whenever you have a question, and keep a record of each call",
+    "Ask your insurance provider for a case-manager",
+    "Ask your insurance provider about covered procedures and pre-approved healthcare workers",
     "Don’t rely on the online information from your insurance provider",
     "Ask for a detailed price estimate of all planned procedures",
+    "Make a list of all hospital staff you plan to receive treatment from",
   ];
   final List<String> subGuidelinesText = [
     "If you know you have a hospital visit coming up, talk to your doctor or healthcare provider about your upcoming hospital visit. Discuss the guidelines 2, 3 and 4 with your provider and take detailed notes of any information you learn.",
-    "Work with your doctor or healthcare provider to make a list of all services, procedures, and health products (casts, blood vials, etc.) you expect to receive during your visit.",
-    "Ask your doctor or healthcare provider to help you write a list of all the hospital staff (doctors, surgeons, assistants, etc.) who you believe will be treating you during your stay. Keep this list; it will help you if you receive a surprise bill later on. ",
-    "Get your lists from guidelines 2 and 3 ready, then call your insurance provider. Tell them about each test, procedure and service you plan to receive and each hospital staff or doctor you plan to see. Ask them to tell you which services, procedures and hospital staff are in-network (covered by your insurance).",
+    "The medical billing expert we spoke with when creating this app recommends that you create a place where you can keep any and all information related to your hospital stay. This can simply be a paper file folder or a box, or a digital folder on your computer for any digital notes, photos, emails, voice recordings, etc. you may have related to your visit.\n\nYou don’t need to be super organized, just make sure you keep any and all documents or information you receive in one place. For example, never throw away any letters that you receive related to your bill, even if you don’t understand them.",
+    "Most of the questions related to your medical bills can be answered by your insurance provider, it is recommended to speak with them when planning your hospital visit.\n\nCall your insurance company while you are planning your visit and discuss the tests and/or procedures you plan to have. If the tests or procedures have already been ordered by your doctor, your insurance company should already have a copy of all the procedures/tests you will receive.\n\nIn order to keep a record of the conversations with your insurance company, ask for a reference number for each call you make, and the name of the person you speak with. This record will help you in future conversations.",
+    "Your insurance company may be able to assign a specific person to your case. This person should be able to answer your questions about what is and is not covered and how much you will pay. You can simply say \“I’d really like to have one person manage my case.\”",
+    "Call your insurance provider and ask them if there are hospital staff at the hospital you plan to stay at who are pre-approved (in-network: covered by your insurance). Also, ask which of the procedures/tests you plan to have are covered.\n\nOnce you know which healthcare providers are in-network, request to be seen by them at the hospital if possible.",
+    "Ask your doctor or healthcare provider and your support person (a trusted friend or family member) to help you write a list of all the hospital staff (doctors, surgeons, assistants, etc.) who you believe will be treating you during your stay. Keep this list; it will help you if you receive a surprise bill later on.",
     "\“Studies have shown that more than half of provider directory information can be incorrect, including the phone number, address, whether the provider is taking patients and who’s considered in-network.\” It is strongly advised to always call your insurance provider before your visit and double check all of your questions with them.",
-    "Ask your insurance provider and the hospital administrators for a detailed price estimate of the procedure that you are planning to have.",
+    "Ask the hospital administrators for a detailed price estimate of the procedure that you are planning to have. Knowing this, paired with the knowledge of how much your insurance will pay vs. how much you have to pay out-of-pocket, you should be able to get a good estimate of the costs of your upcoming visit/s.",
+    "Ask your doctor or healthcare provider to help you write a list of all the hospital staff (doctors, surgeons, assistants, etc.) who you believe will be treating you during your stay. Keep this list; it will help you if you receive a surprise bill later on. ",
   ];
 
   @override
@@ -267,6 +274,8 @@ class BeforeStayPage extends StatelessWidget {
               renderGuideline(guidelinesTitles[3], subGuidelinesText[3], 4, 1),
               renderGuideline(guidelinesTitles[4], subGuidelinesText[4], 5, 1),
               renderGuideline(guidelinesTitles[5], subGuidelinesText[5], 6, 1),
+              renderGuideline(guidelinesTitles[6], subGuidelinesText[6], 7, 1),
+              renderGuideline(guidelinesTitles[7], subGuidelinesText[7], 8, 1),
               ElevatedButton(
                   child: Text("Back"), onPressed: () => Navigator.pop(context)),
             ],
@@ -288,15 +297,21 @@ class DuringStayPage extends StatelessWidget {
   final RootCategoriesPage rootCategoriesPage;
   final List<String> guidelinesTitles = [
     "Ask if you will be treated by in-network physicians/providers",
-    "Do not pay any portion of a large bill if you do not fully understand the charges",
-    "Make a list of every hospital staff member who treats you during your visit",
-    "Take detailed notes of everything you learn during your visit",
+    "Call your insurance provider to ask about costs and what is covered",
+    "Record the names of each hospital staff member who treats you during your visit",
+    "Record everything that happens and everything you learn during your visit",
+    "Do not pay any portion of a bill if you do not fully understand the charges",
+    "Never \“pay out your deductible\” if you are not told the exact amount of your bill",
+    "Don’t rely on all information that the hospitals receptionists or billing department give you",
   ];
   final List<String> subGuidelinesText = [
-    "When you arrive to check-in for your appointment, ask the receptionist to double check that all of the providers you will be seeing during your visit are in-network with your insurance.",
-    "If you are asked to pay any amount for your treatment while you are at the hospital, ask to see an itemized bill describing each of your charges in detail. Never pay any amount until you fully understand each charge. The best option is to until after you leave the hospital before you pay any amount toward your bill or bills. This is because you may be able to negotiate or dispute the bill by writing a simple letter before you pay.",
-    "Write down the names of every hospital staff member who treats you during your visit.",
-    "Take detailed notes of any information that you learn from your healthcare providers during your hospital stay, such as if a certain doctor is in-network or out-of network, medical advice given to you by a nurse or a doctor, over the counter treatments you may try, etc. This will not only help you avoid surprise bills, it will also help you manage your health.",
+    "When you arrive to check-in for your appointment, ask the receptionist to double check that all of the providers you will be seeing during your visit are in-network with your insurance.\n\nEspecially if you will be treated by anesthesiologists or radiologists, which are very commonly contracted by the hospital and potentially out-of-network.",
+    "The best thing to do when you want to find out what is covered under your insurance and how much procedures/services cost is to call your insurance company. They are often the only ones who will be able to tell you exactly what is covered and the amount of the final bill you will likely have to pay.",
+    "Have a trusted friend or family member (support person) help you keep a record of the names and job title of every healthcare worker who takes care of you during your visit. You and your support person can use a simple pen and paper, a notes app on your phone, voice recordings and/or pictures of documents to help you keep track of your healthcare providers.",
+    "Have a close friend or family member help you take chronological notes of everything that happens during your visit, such as “Seen by nurse Susan Smith at 11:10am.” Also, note any information that you learn from your healthcare providers during your hospital stay, such as if a certain doctor is in-network or out-of network, medical advice given to you by a nurse or a doctor, over the counter treatments you may try, etc. This will not only help you avoid surprise bills, it will also help you manage your health.",
+    "If you are asked to pay any amount for your treatment while you are at the hospital, ask to see an itemized bill describing each of your charges in detail. Never pay any amount until you fully understand each charge.\n\nThe best option is to wait until after you leave the hospital before you pay any amount toward your bill or bills. This is because you may be able to negotiate or dispute the bill by writing a simple letter before you pay. See the “I recently visited the hospital” and “I have received a surprise medical bill” sections to learn how you can dispute or negotiate bills before paying.",
+    "Many hospitals have adopted the practice of asking patients to pay out the full amount of their insurance plan’s deductible as payment toward a bill before the patient receives care or before the patient has access to the final bill. You should never agree to pay out your deductible until your insurance company tells you how much your bill will be and/or the hospital gives you your fully itemized bill.\n\nTo learn more about this practice used by hospitals, please read the following article: https://www.verywellhealth.com/paying-deductible-before-receiving-care-4159403",
+    "Unfortunately, many hospital receptionists or billing departments will not know specific policies of your insurance provider, or even how much various services/procedures will cost. Please see guideline number 2 in this section.",
   ];
 
   @override
@@ -311,6 +326,9 @@ class DuringStayPage extends StatelessWidget {
               renderGuideline(guidelinesTitles[1], subGuidelinesText[1], 2, 1),
               renderGuideline(guidelinesTitles[2], subGuidelinesText[2], 3, 1),
               renderGuideline(guidelinesTitles[3], subGuidelinesText[3], 4, 1),
+              renderGuideline(guidelinesTitles[4], subGuidelinesText[4], 5, 1),
+              renderGuideline(guidelinesTitles[5], subGuidelinesText[5], 6, 1),
+              renderGuideline(guidelinesTitles[6], subGuidelinesText[6], 7, 1),
               ElevatedButton(
                   child: Text("Back"), onPressed: () => Navigator.pop(context))
             ],
@@ -517,6 +535,67 @@ class TermsPage extends StatelessWidget {
               renderGuideline(guidelinesTitles[4], subGuidelinesText[4], 5, 2),
               renderGuideline(guidelinesTitles[5], subGuidelinesText[5], 6, 2),
               renderGuideline(guidelinesTitles[6], subGuidelinesText[6], 7, 2),
+              ElevatedButton(
+                  child: Text("Back"), onPressed: () => Navigator.pop(context)),
+            ],
+          ),
+          onPanUpdate: (details) {
+            if (details.delta.dy > 0) {
+              //  User swiped left
+              Navigator.pop(context);
+            }
+          },
+        ));
+  }
+}
+
+class CollectionsPage extends StatelessWidget {
+  CollectionsPage({this.context, this.rootCategoriesPage});
+
+  final BuildContext context;
+  final RootCategoriesPage rootCategoriesPage;
+  final List<String> guidelinesTitles = [
+    "Remember that you have time",
+    "You may want to ask for the debt to be sent back to the hospital’s billing agency",
+    "Send a debt validation letter",
+    "Ask to set up a payment plan and try to negotiate the amount of the bill",
+    "Pay off other debt before your medical debt",
+    "Don’t trade medical debt for other types of debt, or pay off medical bills with a credit card",
+  ];
+  final List<String> subGuidelinesText = [
+    ("Remember that most credit reporting agencies wait 180 days from when you are notified of a bill being sent to collections before the debt shows up on your credit report. You can use this time to make a plan for how to handle the debt." +
+        "\n\nhttps://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html"),
+    ("Sometimes hospitals send medical bills to collections agencies prematurely. If your bill was sent to collections before you got a chance to work out a payment plan with the hospital’s billing agency, ask the hospital if they can send the bill back so that you can settle it with them directly." +
+        "\n\nhttps://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html"),
+    ("You have the right to send a debt validation letter/email to the collection agency under the FDCPA asking them to provide you with evidence that the debt you are being asked to pay is in fact yours." +
+        "\n\nIn your letter/email, simply state that you will need to validate the debt before you can pay, and ask for documentation bearing your signature and dates of services you are being billed for. Either send the letter via certified mail or via email, so that you can record the date when the agency receives it." +
+        "\n\nWhat you will do next depends on how the agency responds." +
+        "\n\n(a) They do not respond within 30 days of receiving your request. If they do not respond, you can file a complaint with the Consumer Financial Protection Bureau here: https://www.consumerfinance.gov/complaint/getting-started/ After filing your complaint, send a Failure to Validate letter/email to the collection agency explaining that they have violated your rights as a consumer to have your debt violated, and that you have the right to sue them for damages to your credit score if the debt is not validated, or if the unvalidated debt is not removed from your credit report. Here is a template Failure to Validate letter: https://038b81d3-e365-410b-aae4-6abfa16bcab5.filesusr.com/ugd/ecc7c0_0178390f990542608eec0c82640cb513.pdf" +
+        "\n\n(b) They send you a bill. If they do not send you the explanation of charges and proof that the debt belongs to you, you can also report them to the CFPB and send a Failure to Validate email/letter, because they did not provide sufficient documentation to validate the debt. If they respond within 30 days of receiving your request, do not report them to CFPB yet, but inform them that if they do not send you evidence to validate the debt, you will report them. See (a) and the source blog and video linked below." +
+        "\n\n (c) They send you documentation containing your medical records, (information about procedures you received, doctors you were seen by, etc.). If they do this, immediately file a complaint with the Consumer Financial Protection Bureau for violating your rights under HIPAA. You never signed any agreement authorizing them to access your medical records, so if they obtain this information, they have violated your privacy rights under HIPAA. At this point, you have the option to sue them if you wish, but at the very least, they must remove the debt and cease collection of the debt if they have violated your HIPAA rights. Ask the collections agency to delete your private medical information immediately and to cease all collection activities." +
+        "\n\n Source (Achieving Credit Excellence blog):\n https://www.achievingcreditexcellence.com/freebies\nhttps://www.youtube.com/watch?v=3xVGZ4QOCmk&ab_channel=LifeWithMC"),
+    ("After you have disputed your bill, if you were not able to get the debt removed, set up a payment plan with the collection agency so that you can pay small amounts toward your bill over time. After you have paid a bit of the bill off, you will be on good terms with the agency, and you should ask have your debt lowered/negotiated to an amount that you can afford."),
+    ("It may seem counterintuitive, but AARP recommends that you prioritize paying off other types of debt, such as car loans, credit card bills etc. before paying off your medical debt. This is because medical debt is less damaging to your credit score than other types of debt." +
+        "https://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html"),
+    ("The AARP warns patients not to trade medical debt for other types of debt; i.e. do not take out a loan to pay off your debt, or pay off your medical bills with a credit card. This is because medical debt typically does not have late fees or interest associated with it, while other forms of debt do. Medical debt also takes longer to appear on your credit report than other forms of debt, giving you more time to take care of it." +
+        "https://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.deepPurple[600],
+        body: GestureDetector(
+          child: ListView(
+            children: [
+              renderSituationBox(
+                  "My medical bill/debt has been sent to a collections agency"),
+              renderGuideline(guidelinesTitles[0], subGuidelinesText[0], 1, 2),
+              renderGuideline(guidelinesTitles[1], subGuidelinesText[1], 2, 2),
+              renderGuideline(guidelinesTitles[2], subGuidelinesText[2], 3, 2),
+              renderGuideline(guidelinesTitles[3], subGuidelinesText[3], 4, 2),
+              renderGuideline(guidelinesTitles[4], subGuidelinesText[4], 5, 2),
+              renderGuideline(guidelinesTitles[5], subGuidelinesText[5], 6, 2),
               ElevatedButton(
                   child: Text("Back"), onPressed: () => Navigator.pop(context)),
             ],
