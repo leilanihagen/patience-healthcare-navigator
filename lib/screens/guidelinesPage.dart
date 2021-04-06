0df699e9158f98f249815da3a84c1eda32c62395
Leilani Hagen <hagen.leilani@gmail.com>
2021-04-06 11:06:49 -0700

    DuringStay hyperlink added.

--- lib/screens/guidelinesPage.dart
+++ lib/screens/guidelinesPage.dart
@@ -558,11 +558,17 @@ class DuringStayPage extends StatelessWidget {
     "Have a trusted friend or family member (support person) help you keep a record of the names and job title of every healthcare worker who takes care of you during your visit. You and your support person can use a simple pen and paper, a notes app on your phone, voice recordings and/or pictures of documents to help you keep track of your healthcare providers.",
     "Have a close friend or family member help you take chronological notes of everything that happens during your visit, such as “Seen by nurse Susan Smith at 11:10am.” Also, note any information that you learn from your healthcare providers during your hospital stay, such as if a certain doctor is in-network or out-of network, medical advice given to you by a nurse or a doctor, over the counter treatments you may try, etc. This will not only help you avoid surprise bills, it will also help you manage your health.",
     "If you are asked to pay any amount for your treatment while you are at the hospital, ask to see an itemized bill describing each of your charges in detail. Never pay any amount until you fully understand each charge.\n\nThe best option is to wait until after you leave the hospital before you pay any amount toward your bill or bills. This is because you may be able to negotiate or dispute the bill by writing a simple letter before you pay. See the “I recently visited the hospital” and “I have received a surprise medical bill” sections to learn how you can dispute or negotiate bills before paying.",
-    "Many hospitals have adopted the practice of asking patients to pay out the full amount of their insurance plan’s deductible as payment toward a bill before the patient receives care or before the patient has access to the final bill. You should never agree to pay out your deductible until your insurance company tells you how much your bill will be and/or the hospital gives you your fully itemized bill.\n\nTo learn more about this practice used by hospitals, please read the following article: https://www.verywellhealth.com/paying-deductible-before-receiving-care-4159403",
-    "Unfortunately, many hospital receptionists or billing departments will not know specific policies of your insurance provider, or even how much various services/procedures will cost. Please see guideline number 2 in this section.",
+    "Many hospitals have adopted the practice of asking patients to pay out the full amount of their insurance plan’s deductible as payment toward a bill before the patient receives care or before the patient has access to the final bill. You should never agree to pay out your deductible until your insurance company tells you how much your bill will be and/or the hospital gives you your fully itemized bill.\n\nTo learn more about this practice used by hospitals, please read the source article below.",
+    "Unfortunately, many hospital receptionists or billing departments will not know specific policies of your insurance provider, or even how much various services/procedures will cost.\n\nPlease see guideline number 2 in this section.",
   ];
 
-  String 
+  final List<String> hyperlinkHintsText = [
+    "Source (verywellhealth.com article)",
+  ];
+
+  final List<String> hyperlinks = [
+    "https://www.verywellhealth.com/paying-deductible-before-receiving-care-4159403",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -580,7 +586,13 @@ class DuringStayPage extends StatelessWidget {
               renderGuideline(guidelinesTitles[2], subGuidelinesText[2], 3, 0),
               renderGuideline(guidelinesTitles[3], subGuidelinesText[3], 4, 0),
               renderGuideline(guidelinesTitles[4], subGuidelinesText[4], 5, 0),
-              renderGuideline(guidelinesTitles[5], subGuidelinesText[5], 6, 0),
+              renderGuidelineHyperlink(
+                  guidelinesTitles[5],
+                  subGuidelinesText[5],
+                  hyperlinkHintsText[0],
+                  hyperlinks[0],
+                  6,
+                  0),
               renderGuideline(guidelinesTitles[6], subGuidelinesText[6], 7, 0),
               ElevatedButton(
                 child: ListTile(
