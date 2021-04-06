125a1f901839c2fa7fcc5f43f1de1088d61e5c07
Leilani Hagen <hagen.leilani@gmail.com>
2021-04-06 12:38:07 -0700

    CollectionsPage hyperlink added.

--- lib/screens/guidelinesPage.dart
+++ lib/screens/guidelinesPage.dart
@@ -746,7 +746,7 @@ class ReceivedBillPage extends StatelessWidget {
     "https://brokenhealthcare.org/dispute-every-hospital-bill/",
   ];
 
-  final Column text =
+  final Column guideline4Text =
       Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
     Text(
       ("Many medical bills sent to patients are confusing and lack sufficient detail for the charges to be well-understood by the patient.\n\nHave a close friend or family member help you write an email (preferred, if possible) or letter to the hospital explaining that you will need more detailed information describing your charges and how they were calculated before you can pay. Try not to worry about sounding professional, just describe what you need from the hospital in simple language.\n\nElements to include in your letter:\n\n(a) Ask in your letter that the billing agency and/or hospital explain to you how each individual charge was determined.\n\n(b) Ask that they provide you with an itemized bill for each date of service they are charging you for.\n\n(c) Once you receive the itemized bill and an explanation of the hospitals charges, you will want to compare this information to the information in your insurance plan’s Explanation of Benefits form.\n\n(d) Ask that they show you evidence that they complied with federal and state pricing laws (in your state).\n\n(e) State that you will pay the correct amount according to your insurance plan’s Explanation of Benefits form after the hospital provides you with all requested information. You may state that it is your right as a consumer to have detailed knowledge about what exactly you are paying for.\n\n(f) Finally, state that you are disputing your bill and that you wish to have your account marked as disputed. State that if your bill is sent to a collections agency, you wish that the hospital/billing agency include a copy of all correspondence between yourself and the hospital, and that the hospital marks your file as disputed." +
@@ -814,7 +814,8 @@ class ReceivedBillPage extends StatelessWidget {
               renderGuideline(guidelinesTitles[0], subGuidelinesText[0], 1, 0),
               renderGuideline(guidelinesTitles[1], subGuidelinesText[1], 2, 0),
               renderGuideline(guidelinesTitles[2], subGuidelinesText[2], 3, 0),
-              renderGuidelineCustomWidgetText(guidelinesTitles[3], text, 4, 0),
+              renderGuidelineCustomWidgetText(
+                  guidelinesTitles[3], guideline4Text, 4, 0),
               ElevatedButton(
                 child: ListTile(
                     leading: Icon(Icons.arrow_back_ios_rounded,
@@ -865,23 +866,88 @@ class CollectionsPage extends StatelessWidget {
     "Don’t trade medical debt for other types of debt, or pay off medical bills with a credit card",
   ];
   final List<String> subGuidelinesText = [
-    ("Remember that most credit reporting agencies wait 180 days from when you are notified of a bill being sent to collections before the debt shows up on your credit report. You can use this time to make a plan for how to handle the debt." +
-        "\n\nhttps://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html"),
-    ("Sometimes hospitals send medical bills to collections agencies prematurely. If your bill was sent to collections before you got a chance to work out a payment plan with the hospital’s billing agency, ask the hospital if they can send the bill back so that you can settle it with them directly." +
-        "\n\nhttps://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html"),
-    ("You have the right to send a debt validation letter/email to the collection agency under the FDCPA asking them to provide you with evidence that the debt you are being asked to pay is in fact yours." +
-        "\n\nIn your letter/email, simply state that you will need to validate the debt before you can pay, and ask for documentation bearing your signature and dates of services you are being billed for. Either send the letter via certified mail or via email, so that you can record the date when the agency receives it." +
-        "\n\nWhat you will do next depends on how the agency responds." +
-        "\n\n(a) They do not respond within 30 days of receiving your request. If they do not respond, you can file a complaint with the Consumer Financial Protection Bureau here: https://www.consumerfinance.gov/complaint/getting-started/ After filing your complaint, send a Failure to Validate letter/email to the collection agency explaining that they have violated your rights as a consumer to have your debt violated, and that you have the right to sue them for damages to your credit score if the debt is not validated, or if the unvalidated debt is not removed from your credit report. Here is a template Failure to Validate letter: https://038b81d3-e365-410b-aae4-6abfa16bcab5.filesusr.com/ugd/ecc7c0_0178390f990542608eec0c82640cb513.pdf" +
-        "\n\n(b) They send you a bill. If they do not send you the explanation of charges and proof that the debt belongs to you, you can also report them to the CFPB and send a Failure to Validate email/letter, because they did not provide sufficient documentation to validate the debt. If they respond within 30 days of receiving your request, do not report them to CFPB yet, but inform them that if they do not send you evidence to validate the debt, you will report them. See (a) and the source blog and video linked below." +
-        "\n\n (c) They send you documentation containing your medical records, (information about procedures you received, doctors you were seen by, etc.). If they do this, immediately file a complaint with the Consumer Financial Protection Bureau for violating your rights under HIPAA. You never signed any agreement authorizing them to access your medical records, so if they obtain this information, they have violated your privacy rights under HIPAA. At this point, you have the option to sue them if you wish, but at the very least, they must remove the debt and cease collection of the debt if they have violated your HIPAA rights. Ask the collections agency to delete your private medical information immediately and to cease all collection activities." +
-        "\n\n Source (Achieving Credit Excellence blog):\n https://www.achievingcreditexcellence.com/freebies\nhttps://www.youtube.com/watch?v=3xVGZ4QOCmk&ab_channel=LifeWithMC"),
-    ("After you have disputed your bill, if you were not able to get the debt removed, set up a payment plan with the collection agency so that you can pay small amounts toward your bill over time. After you have paid a bit of the bill off, you will be on good terms with the agency, and you should ask have your debt lowered/negotiated to an amount that you can afford."),
-    ("It may seem counterintuitive, but AARP recommends that you prioritize paying off other types of debt, such as car loans, credit card bills etc. before paying off your medical debt. This is because medical debt is less damaging to your credit score than other types of debt." +
-        "https://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html"),
-    ("The AARP warns patients not to trade medical debt for other types of debt; i.e. do not take out a loan to pay off your debt, or pay off your medical bills with a credit card. This is because medical debt typically does not have late fees or interest associated with it, while other forms of debt do. Medical debt also takes longer to appear on your credit report than other forms of debt, giving you more time to take care of it." +
-        "https://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html")
+    "Remember that most credit reporting agencies wait 180 days from when you are notified of a bill being sent to collections before the debt shows up on your credit report. You can use this time to make a plan for how to handle the debt.",
+    "Sometimes hospitals send medical bills to collections agencies prematurely. If your bill was sent to collections before you got a chance to work out a payment plan with the hospital’s billing agency, ask the hospital if they can send the bill back so that you can settle it with them directly.",
+    // ("You have the right to send a debt validation letter/email to the collection agency under the FDCPA asking them to provide you with evidence that the debt you are being asked to pay is in fact yours." +
+    //     "\n\nIn your letter/email, simply state that you will need to validate the debt before you can pay, and ask for documentation bearing your signature and dates of services you are being billed for. Either send the letter via certified mail or via email, so that you can record the date when the agency receives it." +
+    //     "\n\nWhat you will do next depends on how the agency responds." +
+    //     "\n\n(a) They do not respond within 30 days of receiving your request. If they do not respond, you can file a complaint with the Consumer Financial Protection Bureau here: https://www.consumerfinance.gov/complaint/getting-started/ After filing your complaint, send a Failure to Validate letter/email to the collection agency explaining that they have violated your rights as a consumer to have your debt violated, and that you have the right to sue them for damages to your credit score if the debt is not validated, or if the unvalidated debt is not removed from your credit report. Here is a template Failure to Validate letter: https://038b81d3-e365-410b-aae4-6abfa16bcab5.filesusr.com/ugd/ecc7c0_0178390f990542608eec0c82640cb513.pdf" +
+    //     "\n\n(b) They send you a bill. If they do not send you the explanation of charges and proof that the debt belongs to you, you can also report them to the CFPB and send a Failure to Validate email/letter, because they did not provide sufficient documentation to validate the debt. If they respond within 30 days of receiving your request, do not report them to CFPB yet, but inform them that if they do not send you evidence to validate the debt, you will report them. See (a) and the source blog and video linked below." +
+    //     "\n\n (c) They send you documentation containing your medical records, (information about procedures you received, doctors you were seen by, etc.). If they do this, immediately file a complaint with the Consumer Financial Protection Bureau for violating your rights under HIPAA. You never signed any agreement authorizing them to access your medical records, so if they obtain this information, they have violated your privacy rights under HIPAA. At this point, you have the option to sue them if you wish, but at the very least, they must remove the debt and cease collection of the debt if they have violated your HIPAA rights. Ask the collections agency to delete your private medical information immediately and to cease all collection activities." +
+    //     "\n\n Source (Achieving Credit Excellence blog):\n https://www.achievingcreditexcellence.com/freebies\nhttps://www.youtube.com/watch?v=3xVGZ4QOCmk&ab_channel=LifeWithMC"),
+    "After you have disputed your bill, if you were not able to get the debt removed, set up a payment plan with the collection agency so that you can pay small amounts toward your bill over time. After you have paid a bit of the bill off, you will be on good terms with the agency, and you should ask have your debt lowered/negotiated to an amount that you can afford.",
+    "It may seem counterintuitive, but AARP recommends that you prioritize paying off other types of debt, such as car loans, credit card bills etc. before paying off your medical debt. This is because medical debt is less damaging to your credit score than other types of debt.",
+    "The AARP warns patients not to trade medical debt for other types of debt; i.e. do not take out a loan to pay off your debt, or pay off your medical bills with a credit card. This is because medical debt typically does not have late fees or interest associated with it, while other forms of debt do. Medical debt also takes longer to appear on your credit report than other forms of debt, giving you more time to take care of it.",
   ];
+
+  final List<String> hyperlinkHintsText = [
+    "Source (aarp.org article)",
+  ];
+
+  final List<String> hyperlinks = [
+    // Guidelines 1,2,5,6 are all accessing the same article:
+    "https://www.aarp.org/money/credit-loans-debt/info-2019/medical-bills-affect-credit-score.html",
+  ];
+
+  final Column guideline3Text =
+      Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+    Text(
+      ("You have the right to send a debt validation letter/email to the collection agency under the FDCPA asking them to provide you with evidence that the debt you are being asked to pay is in fact yours." +
+          "\n\nIn your letter/email, simply state that you will need to validate the debt before you can pay, and ask for documentation bearing your signature and dates of services you are being billed for. Either send the letter via certified mail or via email, so that you can record the date when the agency receives it." +
+          "\n\nWhat you will do next depends on how the agency responds." +
+          "\n\n(a) They do not respond within 30 days of receiving your request. If they do not respond, you can file a complaint with the Consumer Financial Protection Bureau "),
+      style: TextStyle(fontSize: 17),
+    ),
+    InkWell(
+        child: Text(
+          "here (consumerfinance.gov complaint submission).\n\n",
+          style: TextStyle(
+              fontSize: 17,
+              color: HexColor("#758C20"),
+              decoration: TextDecoration.underline),
+        ),
+        onTap: () => launch(
+            "https://www.consumerfinance.gov/complaint/getting-started/")),
+    Text(
+      "After filing your complaint, send a Failure to Validate letter/email to the collection agency explaining that they have violated your rights as a consumer to have your debt violated, and that you have the right to sue them for damages to your credit score if the debt is not validated, or if the unvalidated debt is not removed from your credit report.",
+      style: TextStyle(fontSize: 17),
+    ),
+    InkWell(
+        child: Text(
+          "\n\nHere is a template Failure to Validate letter (from the achievingcreditexcellence.com blog).",
+          style: TextStyle(
+              fontSize: 17,
+              color: HexColor("#758C20"),
+              decoration: TextDecoration.underline),
+        ),
+        onTap: () =>
+            launch("https://www.achievingcreditexcellence.com/freebies")),
+    Text(
+      "\n\n(b) They send you a bill. If they do not send you the explanation of charges and proof that the debt belongs to you, you can also report them to the CFPB and send a Failure to Validate email/letter, because they did not provide sufficient documentation to validate the debt. If they respond within 30 days of receiving your request, do not report them to CFPB yet, but inform them that if they do not send you evidence to validate the debt, you will report them. See (a) and the source blog and video linked below." +
+          "\n\n (c) They send you documentation containing your medical records, (information about procedures you received, doctors you were seen by, etc.). If they do this, immediately file a complaint with the Consumer Financial Protection Bureau for violating your rights under HIPAA. You never signed any agreement authorizing them to access your medical records, so if they obtain this information, they have violated your privacy rights under HIPAA. At this point, you have the option to sue them if you wish, but at the very least, they must remove the debt and cease collection of the debt if they have violated your HIPAA rights. Ask the collections agency to delete your private medical information immediately and to cease all collection activities.",
+      style: TextStyle(fontSize: 17),
+    ),
+    InkWell(
+        child: Text(
+          "\n\nSource (from the achievingcreditexcellence.com blog).",
+          style: TextStyle(
+              fontSize: 17,
+              color: HexColor("#758C20"),
+              decoration: TextDecoration.underline),
+        ),
+        onTap: () =>
+            launch("https://www.achievingcreditexcellence.com/freebies")),
+    InkWell(
+        child: Text(
+          "\n\nSource (YouTube Video explaining how to dispute the debt).",
+          style: TextStyle(
+              fontSize: 17,
+              color: HexColor("#758C20"),
+              decoration: TextDecoration.underline),
+        ),
+        onTap: () => launch(
+            "https://www.youtube.com/watch?v=3xVGZ4QOCmk&ab_channel=LifeWithMC")),
+  ]);
 
   @override
   Widget build(BuildContext context) {
@@ -894,12 +960,40 @@ class CollectionsPage extends StatelessWidget {
                   "My medical bill/debt has been sent to a collections agency",
                   Icon(Icons.priority_high_rounded,
                       color: HexColor(blueTheme), size: 33)),
-              renderGuideline(guidelinesTitles[0], subGuidelinesText[0], 1, 0),
-              renderGuideline(guidelinesTitles[1], subGuidelinesText[1], 2, 0),
-              renderGuideline(guidelinesTitles[2], subGuidelinesText[2], 3, 0),
-              renderGuideline(guidelinesTitles[3], subGuidelinesText[3], 4, 0),
-              renderGuideline(guidelinesTitles[4], subGuidelinesText[4], 5, 0),
-              renderGuideline(guidelinesTitles[5], subGuidelinesText[5], 6, 0),
+              renderGuidelineHyperlink(
+                  guidelinesTitles[0],
+                  subGuidelinesText[0],
+                  hyperlinkHintsText[0],
+                  hyperlinks[0],
+                  1,
+                  0),
+              renderGuidelineHyperlink(
+                  guidelinesTitles[1],
+                  subGuidelinesText[1],
+                  hyperlinkHintsText[0],
+                  hyperlinks[0],
+                  2,
+                  0),
+              renderGuidelineCustomWidgetText(
+                  guidelinesTitles[2], guideline3Text, 3, 0),
+              renderGuideline(guidelinesTitles[3], subGuidelinesText[2], 4, 0),
+              renderGuidelineHyperlink(
+                  guidelinesTitles[4],
+                  subGuidelinesText[3],
+                  hyperlinkHintsText[0],
+                  hyperlinks[0],
+                  5,
+                  0),
+              renderGuidelineHyperlink(
+                  guidelinesTitles[5],
+                  subGuidelinesText[4],
+                  hyperlinkHintsText[0],
+                  hyperlinks[0],
+                  6,
+                  0),
+              // renderGuideline(guidelinesTitles[3], subGuidelinesText[3], 4, 0),
+              // renderGuideline(guidelinesTitles[4], subGuidelinesText[4], 5, 0),
+              // renderGuideline(guidelinesTitles[5], subGuidelinesText[5], 6, 0),
               ElevatedButton(
                 child: ListTile(
                     leading: Icon(Icons.arrow_back_ios_rounded,
