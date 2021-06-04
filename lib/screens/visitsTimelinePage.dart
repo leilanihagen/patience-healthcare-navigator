54d968a24e6f5076096c19183fb1e2ab5621b73f
Leilani Hagen <hagen.leilani@gmail.com>
2021-06-04 16:11:02 -0700

    Note formatting IP. Add media buttons IP.

--- lib/screens/visitsTimelinePage.dart
+++ lib/screens/visitsTimelinePage.dart
@@ -17,45 +17,75 @@ class VisitsTimelinePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: HexColor(purpleTheme),
-        body: Container(
-            margin:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            padding: const EdgeInsets.all(20.0),
-            height: 200, // TODO: make dynamic
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(20.0)),
-            child: Column(
-              // Title and note body will be contained within this:
-
+        body: Column(
+          children: [
+            // Add media buttons:
+            Row(
               children: [
-                Row(
-                  // Title line:
-                  children: [
-                    // Note title:
-                    Expanded(
-                        // TODO: Replace placeholder:
-                        child: Text('Untitled')),
-
-                    // Note date/time:
-                    Container(
-                        child: Column(
-                      children: [
-                        // TODO: Replace placeholders:
-                        RichText(
-                            text: TextSpan(
-                          text: '12:26 PM',
-                          style: TextStyle(color: Colors.black),
-                        )),
-                        RichText(
-                            text: TextSpan(
-                          text: '6 April 2021',
-                          style: TextStyle(color: Colors.black),
-                        )),
-                      ],
-                    ))
-                  ],
-                )
+                IconButton(
+                    icon: Icon(Icons.camera_alt),
+                    color: Colors.white,
+                    onPressed: () => {}),
+                IconButton(
+                    icon: Icon(Icons.mic),
+                    color: Colors.white,
+                    onPressed: () => {})
               ],
-            )));
+            ),
+
+            // Single note (EXAMPLE):
+            Container(
+                margin: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 10.0),
+                padding: const EdgeInsets.all(20.0),
+                height: 200, // TODO: make dynamic
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: Column(
+                  // Title and note body will be contained within this:
+
+                  children: [
+                    // Title line:
+                    Row(
+                      children: [
+                        // Note title:
+                        Expanded(
+                            // TODO: Replace placeholder:
+                            child: RichText(
+                                text: TextSpan(
+                                    text: 'Untitled',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline6))),
+
+                        // Note date/time:
+                        Container(
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(),
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Column(
+                              children: [
+                                // TODO: Replace placeholders:
+                                RichText(
+                                    text: TextSpan(
+                                  text: '12:26 PM',
+                                  style: TextStyle(color: Colors.black),
+                                )),
+                                RichText(
+                                    text: TextSpan(
+                                  text: '6 April 2021',
+                                  style: TextStyle(color: Colors.black),
+                                )),
+                              ],
+                            ))
+                      ],
+                    )
+                  ],
+                )),
+          ],
+        ));
   }
 }
