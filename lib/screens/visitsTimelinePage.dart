077b898ebb1ac02cefac93c65978e5e64e1cc46a
Leilani Hagen <hagen.leilani@gmail.com>
2021-06-04 20:01:49 -0700

    + Black theme/borders visit UI mockup.

--- lib/screens/visitsTimelinePage.dart
+++ lib/screens/visitsTimelinePage.dart
@@ -22,17 +22,115 @@ class VisitsTimelinePage extends StatelessWidget {
         body: Column(
           // Render each visit:
           children: [
-            // Visit (EXAMPLE):
             GestureDetector(
               onTap: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => VisitDetailPage())),
+
+              // Visit (EXAMPLE):
               child: Container(
                 margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
                 alignment: Alignment.topCenter,
                 decoration: BoxDecoration(
-                    color: Colors.white,
+                    color: HexColor(lightGreenTheme),
                     borderRadius: BorderRadius.circular(5.0)),
-                height: 240,
+                height: 310,
+                child: Column(
+                  children: [
+                    // Visit info line:
+                    Row(
+                      children: [
+                        // Date:
+                        Container(
+                            alignment: Alignment.centerLeft,
+                            padding: EdgeInsets.all(5.0),
+                            margin: EdgeInsets.all(7.0),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(8.0)),
+                            height: 32.0,
+                            width: 120.0,
+
+                            //Text:
+                            child: Text(
+                              "Today",
+                              textAlign: TextAlign.center,
+                            ))
+                      ],
+                    ),
+
+                    // Note:
+                    Column(
+                      children: [
+                        Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 10.0),
+                            padding: const EdgeInsets.all(20.0),
+                            height: 200, // TODO: make dynamic
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(20.0)),
+                            child: Column(
+                              // Title and note body will be contained within this:
+
+                              children: [
+                                // Title line:
+                                Row(
+                                  children: [
+                                    // Note title:
+                                    Expanded(
+                                        // TODO: Replace placeholder:
+                                        child: RichText(
+                                            text: TextSpan(
+                                                text: 'Untitled',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline6))),
+
+                                    // Note date/time:
+                                    Container(
+                                        padding: EdgeInsets.all(8.0),
+                                        decoration: BoxDecoration(
+                                          border: Border.all(),
+                                          borderRadius:
+                                              BorderRadius.circular(20.0),
+                                        ),
+                                        child: Column(
+                                          children: [
+                                            // TODO: Replace placeholders:
+                                            RichText(
+                                                text: TextSpan(
+                                              text: '12:26 PM',
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )),
+                                            RichText(
+                                                text: TextSpan(
+                                              text: '6 April 2021',
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            )),
+                                          ],
+                                        ))
+                                  ],
+                                )
+                              ],
+                            )),
+                      ],
+                    ),
+
+                    // "More" icon:
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Icon(
+                        Icons.more_horiz,
+                        color: Colors.black,
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
