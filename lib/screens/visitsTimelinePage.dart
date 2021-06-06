7c9da8ca93508f7ef560d3335194815497c7191f
Leilani Hagen <hagen.leilani@gmail.com>
2021-06-06 09:57:54 -0700

    + Visit summaries styling.

--- lib/screens/visitsTimelinePage.dart
+++ lib/screens/visitsTimelinePage.dart
@@ -23,11 +23,11 @@ class _VisitsTimelinePageState extends State<VisitsTimelinePage> {
   // final String darkGreenTheme = "#758C20";
   final String lightGreenTheme = "#A1BF36";
 
-  int visitsCount;
-  List<String> visitsDates = [];
-  List<String> visitsPatients = [];
+  int visitsCount = 3;
+  List<String> visitsDates = ['Today', '03/04/2019', '02/28/2020'];
+  List<String> visitsTimes = ['NOW', '3:26 PM', '12:39 PM'];
+  List<String> visitsPatients = ['Sally', 'Sally', 'Sally'];
   // List<dynamic> notesTitles = [];
-  List<dynamic> notesBodies = [];
 
   // TEST: Note data storage + encoding testing:
   List<List<String>> notesTitles = [
@@ -36,7 +36,13 @@ class _VisitsTimelinePageState extends State<VisitsTimelinePage> {
       'Taken in ambulance to peacehelth',
       'Arrived at ER',
       'Seen by doctor Cylde R.'
-    ]
+    ],
+    ['This hapened']
+  ];
+  List<dynamic> notesBodies = [
+    ['Detail about this happended', 'more detail then this happened'],
+    ['Arrived at x hospital and blah b;ah b', 'Seen by Dr. Strange jdehf[ oei'],
+    ['This happened'],
   ];
   List<dynamic> decodedTitles = [
     ['', ''],
@@ -69,160 +75,13 @@ class _VisitsTimelinePageState extends State<VisitsTimelinePage> {
   }
 
   getVisits() {
-    return ListView.builder(
-        itemCount: visitsCount,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-            alignment: Alignment.topCenter,
-            decoration: BoxDecoration(
-                color: HexColor(lightGreenTheme),
-                borderRadius: BorderRadius.circular(5.0)),
-            height: 310,
-            child: Column(
-              children: [
-                // Visit info line:
-                Row(
-                  children: [
-                    // Date:
-                    Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.all(5.0),
-                        margin: EdgeInsets.all(7.0),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            // border: Border.all(),
-                            borderRadius: BorderRadius.circular(8.0)),
-                        height: 32.0,
-                        width: 120.0,
-
-                        // Text:
-                        child: Text(
-                          "Today",
-                          textAlign: TextAlign.center,
-                        )),
-                    // Patient name:
-                    Container(
-                      alignment: Alignment.centerRight,
-                      padding: EdgeInsets.all(5.0),
-                      margin: EdgeInsets.all(7.0),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          // border: Border.all(),
-                          borderRadius: BorderRadius.circular(8.0)),
-                      height: 32.0,
-                      width: 120.0,
-                    ),
-                  ],
-                ),
-
-                // Note:
-                Column(
-                  children: [
-                    Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 10.0),
-                        padding: const EdgeInsets.all(20.0),
-                        height: 200, // TODO: make dynamic
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            // border: Border.all(),
-                            borderRadius: BorderRadius.circular(20.0)),
-                        child: Column(
-                          // Title and note body will be contained within this:
-
-                          children: [
-                            // Title line:
-                            Row(
-                              children: [
-                                // Note title:
-                                Expanded(
-                                    // TODO: Replace placeholder:
-                                    child: RichText(
-                                        text: TextSpan(
-                                            text: 'Untitled',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline6))),
-
-                                // Note date/time:
-                                Container(
-                                    padding: EdgeInsets.all(8.0),
-                                    decoration: BoxDecoration(
-                                      // border: Border.all(),
-                                      borderRadius: BorderRadius.circular(20.0),
-                                    ),
-                                    child: Column(
-                                      children: [
-                                        // TODO: Replace placeholders:
-                                        RichText(
-                                            text: TextSpan(
-                                          text: '12:26 PM',
-                                          style: TextStyle(color: Colors.black),
-                                        )),
-                                        RichText(
-                                            text: TextSpan(
-                                          text: '6 April 2021',
-                                          style: TextStyle(color: Colors.black),
-                                        )),
-                                      ],
-                                    ))
-                              ],
-                            )
-                          ],
-                        )),
-                  ],
-                ),
-
-                // "More" icon:
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Icon(
-                    Icons.more_horiz,
-                    color: Colors.white,
-                    size: 35,
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: HexColor(purpleTheme),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: HexColor(blueTheme),
-          child: Icon(Icons.add),
-        ),
-        body: Column(
-          // Render each visit:
-          children: [
-            // TEMP TEST: button to load notes:
-            // ElevatedButton(
-            //     onPressed: performStoreReadTest,
-            //     child: Text('Press to store data')),
-            // Container(
-            //   color: Colors.white,
-            //   height: 50,
-            //   width: 400,
-            //   child: Column(
-            //     children: [
-            //       Text(decodedTitles[0][0]),
-            //       Text(decodedTitles[0][1]),
-            //       Text(decodedTitles[0][2]),
-            //     ],
-            //   ),
-            // ),
-
-            GestureDetector(
-              onTap: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => VisitDetailPage())),
-
-              // Visit (EXAMPLE):
-              child: Container(
+    return GestureDetector(
+        onTap: () => Navigator.push(context,
+            MaterialPageRoute(builder: (context) => VisitDetailPage())),
+        child: ListView.builder(
+            itemCount: visitsCount,
+            itemBuilder: (context, index) {
+              return Container(
                 margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
                 alignment: Alignment.topCenter,
                 decoration: BoxDecoration(
@@ -246,23 +105,28 @@ class _VisitsTimelinePageState extends State<VisitsTimelinePage> {
                             height: 32.0,
                             width: 120.0,
 
-                            // Text:
+                            // Date text:
                             child: Text(
-                              "Today",
+                              '${visitsDates[index]}',
                               textAlign: TextAlign.center,
                             )),
                         // Patient name:
                         Container(
-                          alignment: Alignment.centerRight,
-                          padding: EdgeInsets.all(5.0),
-                          margin: EdgeInsets.all(7.0),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              // border: Border.all(),
-                              borderRadius: BorderRadius.circular(8.0)),
-                          height: 32.0,
-                          width: 120.0,
-                        ),
+                            alignment: Alignment.centerRight,
+                            padding: EdgeInsets.all(5.0),
+                            margin: EdgeInsets.all(7.0),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                // border: Border.all(),
+                                borderRadius: BorderRadius.circular(8.0)),
+                            height: 32.0,
+                            width: 120.0,
+
+                            // Patient text:
+                            child: Text(
+                              '${visitsPatients[index]}',
+                              textAlign: TextAlign.center,
+                            )),
                       ],
                     ),
 
@@ -271,8 +135,8 @@ class _VisitsTimelinePageState extends State<VisitsTimelinePage> {
                       children: [
                         Container(
                             margin: const EdgeInsets.symmetric(
-                                horizontal: 20.0, vertical: 10.0),
-                            padding: const EdgeInsets.all(20.0),
+                                horizontal: 8.0, vertical: 5.0),
+                            padding: const EdgeInsets.all(15.0),
                             height: 200, // TODO: make dynamic
                             decoration: BoxDecoration(
                                 color: Colors.white,
@@ -284,44 +148,61 @@ class _VisitsTimelinePageState extends State<VisitsTimelinePage> {
                               children: [
                                 // Title line:
                                 Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
                                     // Note title:
                                     Expanded(
-                                        // TODO: Replace placeholder:
+                                        flex: 2,
                                         child: RichText(
                                             text: TextSpan(
-                                                text: 'Untitled',
+                                                text:
+                                                    '${notesTitles[index][0]}',
                                                 style: Theme.of(context)
                                                     .textTheme
                                                     .headline6))),
 
                                     // Note date/time:
-                                    Container(
-                                        padding: EdgeInsets.all(8.0),
-                                        decoration: BoxDecoration(
-                                          // border: Border.all(),
-                                          borderRadius:
-                                              BorderRadius.circular(20.0),
-                                        ),
-                                        child: Column(
-                                          children: [
-                                            // TODO: Replace placeholders:
-                                            RichText(
-                                                text: TextSpan(
-                                              text: '12:26 PM',
-                                              style: TextStyle(
-                                                  color: Colors.black),
-                                            )),
-                                            RichText(
-                                                text: TextSpan(
-                                              text: '6 April 2021',
-                                              style: TextStyle(
-                                                  color: Colors.black),
-                                            )),
-                                          ],
-                                        ))
+                                    Expanded(
+                                        child: Container(
+                                            padding: EdgeInsets.all(8.0),
+                                            decoration: BoxDecoration(
+                                              // border: Border.all(),
+                                              borderRadius:
+                                                  BorderRadius.circular(20.0),
+                                              border: Border.all(),
+                                            ),
+                                            child: Column(
+                                              children: [
+                                                // TODO: Replace placeholders:
+                                                RichText(
+                                                    text: TextSpan(
+                                                  text: '${visitsTimes[index]}',
+                                                  style: TextStyle(
+                                                      color: Colors.black),
+                                                )),
+                                                RichText(
+                                                    text: TextSpan(
+                                                  text: '${visitsDates[index]}',
+                                                  style: TextStyle(
+                                                      color: Colors.black),
+                                                )),
+                                              ],
+                                            )))
                                   ],
-                                )
+                                ),
+
+                                // Note body:
+                                Container(
+                                    alignment: Alignment.centerLeft,
+                                    margin: EdgeInsets.all(5.0),
+                                    padding: EdgeInsets.all(5.0),
+                                    // Note text:
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: '${notesBodies[index][0]}',
+                                      ),
+                                    ))
                               ],
                             )),
                       ],
@@ -338,9 +219,18 @@ class _VisitsTimelinePageState extends State<VisitsTimelinePage> {
                     ),
                   ],
                 ),
-              ),
-            ),
-          ],
-        ));
+              );
+            }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: HexColor(purpleTheme),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: HexColor(blueTheme),
+          child: Icon(Icons.add),
+        ),
+        body: getVisits());
   }
 }
