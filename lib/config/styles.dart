e723beb8c8cea34a9726fd87e57a42be38477c5e
Leilani Hagen <hagen.leilani@gmail.com>
2021-06-22 17:06:14 -0700

    Working on dashboard structure IP, + added styling config file.

---
+++ lib/config/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Styles {
+  static const articleHeading1White =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white);
+}
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hospital_stay_helper/config/styles.dart';
 
 class DashboardPage extends StatefulWidget {
   DashboardPage({Key key}) : super(key: key);
@@ -17,23 +18,49 @@ class _DashboardPageState extends State<DashboardPage> {
   // final String darkGreenTheme = "#758C20";
   final String lightGreenTheme = "#A1BF36";
 
-  Widget buildWalkthroughCard() {
+  Widget buildWalkthroughCard(Image image, String title, String body) {
     return Container(
         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         padding: EdgeInsets.all(10),
         height: 200,
         decoration: BoxDecoration(
             color: HexColor(lightGreenTheme),
-            borderRadius: BorderRadius.circular(20.0))
+            borderRadius: BorderRadius.circular(20.0)),
+        child: Row(
+          children: [
+            // Image:
+            Image(image: )
+            // Text:
+
+            // Button (optional):
+          ],
+        ),
         // gradient: LinearGradient(
         //     colors: [Colors.white12, HexColor(lightGreenTheme)])),
         );
   }
 
+  Padding buildTitle(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      child: Text(
+        text,
+        textAlign: TextAlign.left,
+        style: Styles.articleHeading1White,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Text("Welcome to Patience!"), buildWalkthroughCard()],
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildTitle("Welcome to Patience!"),
+        buildWalkthroughCard(),
+        buildTitle("My Tools"),
+      ],
     );
   }
 }
