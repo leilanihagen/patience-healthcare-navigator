bca6b6989d3d1f09fd7d315613c2c87275894090
daemon29 <34591979+daemon29@users.noreply.github.com>
2021-07-28 11:49:50 -0700

    add pages

---
+++ lib/screens/medicalRecordPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MedicalRecord extends StatefulWidget {
+  MedicalRecord({Key key}) : super(key: key);
+
+  @override
+  _MedicalRecordPage createState() => _MedicalRecordPage();
+}
+
+class _MedicalRecordPage extends State<MedicalRecord> {
+  List<MedicalRecord> records;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: null,
+    );
+  }
+}
