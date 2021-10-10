1bef9ad5e494d46f837320e798854f487a1fb2ef
daemon29 <34591979+daemon29@users.noreply.github.com>
2021-10-09 22:44:17 -0700

    add Patience icon

--- lib/components/icons.dart
+++ lib/components/icons.dart
@@ -8,6 +8,8 @@ class CustomIcon {
 
   static const IconData lightbulb =
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData psuLogo =
+  static const IconData psuIcon =
       IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData patienceIcon =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
